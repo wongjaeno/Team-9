73fb2b548710840b193721a91cd39d356b3ea0bc
dawgr0cket <116786289+dawgr0cket@users.noreply.github.com>
2023-09-06 16:23:24 +0800

    as

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="PetMana", page_icon=":skull:")
+
+# === HEADER SECTION ===
+st.subheader("HI I AM KX :wave:")
+st.title("Our hackathon app")
+st.write("This is our tree wellness tracker")
