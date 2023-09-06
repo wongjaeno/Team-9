5f1cb6067a0507ba5b321994dbee7ee0366edd0b
DudeNav0J <jovan.angkaijie@gmail.com>
2023-09-06 14:01:36 +0800

    u

---
+++ navbar.js
@@ -0,0 +1,23 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', ()=>{
+    //toggle nav
+    nav.classList.toggle('nav-active');
+    document.body.classList.toggle('body-active');
+
+    //links animation
+    navLinks.forEach((link, index) => {
+        if (link.style.animation){
+            link.style.animation = '';
+        }
+        else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`; 
+        }
+    })
+
+    //burger animation
+    burger.classList.toggle('toggle');
+});
+
